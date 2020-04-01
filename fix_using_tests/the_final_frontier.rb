3e2dac80635bbc3681d36ae804b9e7b7f89020ee
Omowonuola Jimoh <ojimoh21@live.co.uk>
2020-04-01 18:36:28 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,4 +1,4 @@
- require 'pry'
+ # require 'pry'
 
 def generate_star_date
   (rand(100000) + 400000) / 10.0
@@ -12,5 +12,4 @@ end
 def engage
   date = generate_star_date
   puts state_log(date)
-  binding.pry
 end
